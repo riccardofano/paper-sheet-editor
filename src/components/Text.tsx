54d80c728153a574624dde9a516882bf01a9a34f
Riccardo Fano <rcr.fano@gmail.com>
2024-03-16 14:52:51 +0100

    Suppress contentEditable warning

--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -2,6 +2,7 @@ export default function Text() {
   return (
     <span
       contentEditable
+      suppressContentEditableWarning
       style={{ minWidth: "1rem", border: "1px dashed gray" }}
     >
       Insert your text here

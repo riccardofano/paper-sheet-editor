674451ff724d016ceedc9589f7b2825030df8115
Riccardo Fano <rcr.fano@gmail.com>
2024-03-18 16:47:52 +0100

    Show tile properties as inputs

--- src/components/SettingsPanel.tsx
+++ src/components/SettingsPanel.tsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { ChangeEvent, useContext } from "react";
 import { TilesContext } from "../Tile";
 
 interface SettingsPanelProps {
@@ -8,6 +8,19 @@ interface SettingsPanelProps {
 export default function SettingsPanel({ selectedId }: SettingsPanelProps) {
   const { tiles, setTiles } = useContext(TilesContext);
 
+  function handleChange(e: ChangeEvent<HTMLInputElement>, key: string) {
+    // TODO: some properties have to be changed differently like the type
+    // and basically everything that is not a string
+
+    setTiles((prev) => {
+      const next = [...prev];
+      // @ts-expect-error: The keys provided to the function come from the
+      // object itself already so it shouldn't be a problem to index by them
+      next[selectedId][key] = e.target.value;
+      return next;
+    });
+  }
+
   const selectedTile = tiles[selectedId];
   if (!selectedTile) {
     console.error("Selected tile does not exist");
@@ -16,12 +29,18 @@ export default function SettingsPanel({ selectedId }: SettingsPanelProps) {
 
   return (
     <section style={{ padding: "1rem" }}>
-      {Object.entries(selectedTile).map(([key, value]) => (
-        <label key={key} style={{ display: "block" }}>
-          {key}
-          <input type="text" value={value} />
-        </label>
-      ))}
+      {selectedTile.type !== null &&
+        Object.entries(selectedTile).map(([key, value]) => (
+          <label key={key} style={{ display: "block" }}>
+            {key}
+            <br />
+            <input
+              type="text"
+              value={value}
+              onChange={(e) => handleChange(e, key)}
+            />
+          </label>
+        ))}
     </section>
   );
 }

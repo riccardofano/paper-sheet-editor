3063535283f88ee785661308314f1f23a8d23446
Riccardo Fano <rcr.fano@gmail.com>
2024-03-31 16:50:39 +0200

    Have one paragraph as default in the list already

--- src/Tile.ts
+++ src/Tile.ts
@@ -66,7 +66,7 @@ export function getDefaultProperties(type: TileType | null): TileProperties {
       return {
         ...defaultTileProperties,
         type,
-        paragraphs: [],
+        paragraphs: ["Insert some text here"],
         font: "Merriweather",
       };
     case "Image":
